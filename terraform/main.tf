062af981064684748731723ee3264d2f185be1f3
jeff lo <jeff@ve12sus.com>
2021-10-27 19:23:12 -0400

    cleaned up more comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,6 @@ resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create var.az_count public subnets, each in a different AZ
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.default.id
   count                   = var.az_count
@@ -52,19 +51,16 @@ resource "aws_subnet" "private" {
   availability_zone = data.aws_availability_zones.available.names[count.index] 
 }
 
-# Internet Gateway for the public subnet
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.default.id
 }
 
-# Route the public subnet traffic through the IGW
 resource "aws_route" "internet_access" {
   route_table_id         = aws_vpc.default.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-# Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 resource "aws_eip" "gw" {
   count      = var.az_count
   vpc        = true
@@ -77,7 +73,6 @@ resource "aws_nat_gateway" "gw" {
   allocation_id = element(aws_eip.gw.*.id, count.index)
 }
 
-# Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
   count  = var.az_count
   vpc_id = aws_vpc.default.id
@@ -88,14 +83,12 @@ resource "aws_route_table" "private" {
   }
 }
 
-# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
 resource "aws_route_table_association" "private" {
   count          = var.az_count
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
 }
 
-# ALB Security Group: Edit to restrict access to the application
 resource "aws_security_group" "lb" {
   name        = "gbl-load-balancer-security-group"
   description = "controls access to the ALB"
@@ -116,7 +109,6 @@ resource "aws_security_group" "lb" {
   }
 }
 
-# Traffic to the ECS cluster should only come from the ALB
 resource "aws_security_group" "ecs_tasks" {
   name        = "gbl-ecs-tasks-security-group"
   description = "allow inbound access from the ALB only"
@@ -161,7 +153,6 @@ resource "aws_alb_target_group" "app" {
   }
 }
 
-# Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.main.id
   port              = var.app_port
@@ -204,12 +195,10 @@ resource "aws_ecs_task_definition" "gbl_api" {
 
   execution_role_arn = aws_iam_role.gbl_api_task_execution_role.arn
 
-  # These are the minimum values for Fargate containers.
   cpu = 256
   memory = 512
   requires_compatibilities = ["FARGATE"]
 
-  # This is required for Fargate containers (more on this later).
   network_mode = "awsvpc"
 }
 
@@ -254,19 +243,15 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
   }
 }
 
-# Normally we'd prefer not to hardcode an ARN in our Terraform, but since this is
-# an AWS-managed policy, it's okay.
 data "aws_iam_policy" "ecs_task_execution_role" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Attach the above policy to the execution role.
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.gbl_api_task_execution_role.name
   policy_arn = data.aws_iam_policy.ecs_task_execution_role.arn
 }
 
-# Set up CloudWatch group and log stream and retain logs for 30 days
 resource "aws_cloudwatch_log_group" "gbl_log_group" {
   name              = "/ecs/gbl-app"
   retention_in_days = 30
